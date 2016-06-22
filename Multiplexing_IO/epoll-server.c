7c2819e1b25dc82607fff3e1380473a2aa052601
lookfiresu123 <lookfiresu123@gmail.com>
2016-06-23 01:14:53 +0800

    modify format

--- Multiplexing_IO/epoll-server.c
+++ Multiplexing_IO/epoll-server.c
@@ -15,12 +15,9 @@
 /*
  * 设置句柄为非阻塞方式
  */
-int setnonblocking(int sockfd)
-{
-    if (fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFD, 0)|O_NONBLOCK) == -1) 
-    {
+int setnonblocking(int sockfd) {
+    if (fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFD, 0)|O_NONBLOCK) == -1)
         return -1;
-    }
     return 0;
 }
 
@@ -29,15 +26,13 @@ int setnonblocking(int sockfd)
 * 演示epoll接受海量socket并进行处理响应的方法
 *********************************************************************/
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+
     int listenfd, connfd, epfd, sockfd, nfds, n, curfds;
     socklen_t len;
-    
     struct sockaddr_in my_addr, their_addr;
     unsigned int myport, lisnum;
     char buf[MAXBUF + 1];
-    
     // 声明epoll_event结构体的变量，ev用于注册事件，events数组用于回传要处理的事件
     struct epoll_event ev;
     struct epoll_event events[MAXEPOLLSIZE];
@@ -53,8 +48,7 @@ int main(int argc, char **argv)
         lisnum = 2;
 
     // 开启 socket 监听
-    if ((listenfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) 
-    {
+    if ((listenfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket");
         exit(1);
     } else
@@ -71,15 +65,13 @@ int main(int argc, char **argv)
     else
         my_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(listenfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr)) == -1) 
-    {
+    if (bind(listenfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr)) == -1) {
         perror("bind");
         exit(1);
     } else
         printf("IP 地址和端口绑定成功\n");
 
-    if (listen(listenfd, lisnum) == -1) 
-    {
+    if (listen(listenfd, lisnum) == -1) {
         perror("listen");
         exit(1);
     } else
@@ -90,41 +82,35 @@ int main(int argc, char **argv)
     len = sizeof(struct sockaddr_in);
     ev.events = EPOLLIN|EPOLLET;
     ev.data.fd = listenfd;
-    
+
     // 将listenfd注册到epoll事件
-    if (epoll_ctl(epfd, EPOLL_CTL_ADD, listenfd, &ev) < 0) 
-    {
+    if (epoll_ctl(epfd, EPOLL_CTL_ADD, listenfd, &ev) < 0) {
         fprintf(stderr, "epoll set insertion error: fd=%d\n", listenfd);
         return -1;
     } else
         printf("监听 socket 加入 epoll 成功！\n");
+
     curfds = 1;
-    
-    while (1) 
-    {
+
+    while (1) {
         // 等待有事件发生
         nfds = epoll_wait(epfd, events, curfds, -1);
-	
-        if (nfds == -1)
-	{
+
+        if (nfds == -1) {
             perror("epoll_wait");
             break;
         }
-        
-        // 处理所有事件 
-        for (n = 0; n < nfds; ++n) 
-	{
+
+        // 处理所有事件
+        for (n = 0; n < nfds; ++n) {
 	    // 如果新监测到一个SOCKET用户连接到了绑定的SOCKET端口，建立新的连接
-            if (events[n].data.fd == listenfd) 
-	    {
+            if (events[n].data.fd == listenfd) {
                 len = sizeof(struct sockaddr);
                 connfd = accept(listenfd, (struct sockaddr *) &their_addr, &len);
-                if (connfd < 0) 
-		{
+                if (connfd < 0) {
                     perror("accept");
                     continue;
-                } 
-                else
+                } else
                     printf("有连接来自于： %s:%d， 分配的 socket 为:%d\n", inet_ntoa(their_addr.sin_addr), ntohs(their_addr.sin_port), connfd);
 
                 setnonblocking(connfd);
@@ -133,29 +119,25 @@ int main(int argc, char **argv)
                 ev.events = EPOLLIN | EPOLLET;
                 // 设置用于读操作的文件描述符
                 ev.data.fd = connfd;
-		
+
                 //注册ev
                 epoll_ctl(epfd, EPOLL_CTL_ADD, connfd, &ev);
                 curfds ++;
-            } 
-            else if (events[n].events & EPOLLIN) // 如果是已经连接的用户，并且收到数据，那么进行读入
-	    {	
+            } else if (events[n].events & EPOLLIN) {    // 如果是已经连接的用户，并且收到数据，那么进行读入
                 printf("EPOLLIN\n");
                 if ((sockfd = events[n].data.fd) < 0)
                     continue;
                 int len;
                 bzero(buf, MAXBUF + 1);
-		
+
                 /* 接收客户端的消息 */
                 /*len = read(sockfd, buf, MAXBUF);*/
-		
+
                 len = recv(sockfd, buf, MAXBUF, 0);
                 if (len > 0)
                     printf("%d接收消息成功:'%s'，共%d个字节的数据\n", sockfd, buf, len);
-                else 
-		{
-                    if (len < 0) 
-		    {
+                else {
+                    if (len < 0) {
                         printf("消息接收失败！错误代码是%d，错误信息是'%s'\n", errno, strerror(errno));
                         epoll_ctl(epfd, EPOLL_CTL_DEL, sockfd, &ev);
                         curfds--;
@@ -164,34 +146,32 @@ int main(int argc, char **argv)
                 }
 
                 // 设置用于写操作的文件描述符
-		ev.data.fd = sockfd;
+		        ev.data.fd = sockfd;
                 // 设置用于注册的写操作事件
-		ev.events = EPOLLOUT | EPOLLET;
-		
-                /*修改sockfd上要处理的事件为EPOLLOUT*/
-		epoll_ctl(epfd, EPOLL_CTL_MOD, sockfd, &ev); //修改标识符，等待下一个循环时发送数据，异步处理的精髓!!!!! ?????
-            } 
-            else if (events[n].events & EPOLLOUT) // 如果有数据发送
-	    {
-		printf("EPOLLOUT\n");
-		sockfd = events[n].data.fd;
-		
-		bzero(buf, MAXBUF + 1);
-		strcpy(buf, "Server already processes!");
+		        ev.events = EPOLLOUT | EPOLLET;
 
-		send(sockfd, buf, strlen(buf), 0);
+                /*修改sockfd上要处理的事件为EPOLLOUT*/
+		        epoll_ctl(epfd, EPOLL_CTL_MOD, sockfd, &ev); //修改标识符，等待下一个循环时发送数据，异步处理的精髓!!!!! ?????
+            } else if (events[n].events & EPOLLOUT) {   // 如果有数据发送
+		        printf("EPOLLOUT\n");
+		        sockfd = events[n].data.fd;
+
+		        bzero(buf, MAXBUF + 1);
+		        strcpy(buf, "Server already processes!");
+
+		        send(sockfd, buf, strlen(buf), 0);
 
                 // 设置用于读操作的文件描述符
-		ev.data.fd = sockfd;
+		        ev.data.fd = sockfd;
                 // 设置用于注册的读操作事件
-		ev.events = EPOLLIN | EPOLLET;
-		
+		        ev.events = EPOLLIN | EPOLLET;
+
                 // 修改sockfd上要处理的事件为EPOLIN
-		epoll_ctl(epfd, EPOLL_CTL_MOD, sockfd, &ev);
-	    }
+		        epoll_ctl(epfd, EPOLL_CTL_MOD, sockfd, &ev);
+	        }
         }
     }
-    
+
     close(listenfd);
     return 0;
 }
--- Multiplexing_IO/select-server.c
+++ Multiplexing_IO/select-server.c
@@ -19,18 +19,15 @@
 * ./server 7838 1
 *********************************************************************/
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
+
     int sockfd, new_fd;
     socklen_t len;
     struct sockaddr_in my_addr, their_addr;
-    
     unsigned int myport, lisnum;
     char buf[MAXBUF + 1];
-    
     fd_set rfds;
     struct timeval tv;
-    
     int retval, maxfd = -1;
 
     if (argv[1])
@@ -44,8 +41,7 @@ int main(int argc, char **argv)
         lisnum = 2;
 
     // create an new socket
-    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) 
-    {
+    if ((sockfd = socket(PF_INET, SOCK_STREAM, 0)) == -1) {
         perror("socket");
         exit(1);
     }
@@ -53,112 +49,93 @@ int main(int argc, char **argv)
     bzero(&my_addr, sizeof(my_addr));
     my_addr.sin_family = PF_INET;
     my_addr.sin_port = htons(myport);
-    
+
     if (argv[3])
         my_addr.sin_addr.s_addr = inet_addr(argv[3]);
     else
         my_addr.sin_addr.s_addr = INADDR_ANY;
 
-    if (bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr)) == -1) 
-    {
+    if (bind(sockfd, (struct sockaddr *) &my_addr, sizeof(struct sockaddr)) == -1) {
         perror("bind");
         exit(1);
     }
 
-    if (listen(sockfd, lisnum) == -1) 
-    {
+    if (listen(sockfd, lisnum) == -1) {
         perror("listen");
         exit(1);
     }
 
-    while (1) 
-    {
+    while (1) {
         printf("\n----等待新的连接到来开始新一轮聊天……\n");
-	
+
         len = sizeof(struct sockaddr);
-	
-        if ((new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &len)) == -1) 
-	{
+
+        if ((new_fd = accept(sockfd, (struct sockaddr *) &their_addr, &len)) == -1) {
             perror("accept");
             exit(errno);
-        } 
-        else
-	{
+        } else
             printf("server: got connection from %s, port %d, socket %d\n", inet_ntoa(their_addr.sin_addr), ntohs(their_addr.sin_port), new_fd);
-	}
-	
+
         // 开始处理每个新连接上的数据收发
         printf("\n准备就绪，可以开始聊天了……直接输入消息回车即可发信息给对方\n");
-	
-        while (1) 
-	{
-            // 把集合清空 
+
+        while (1) {
+            // 把集合清空
             FD_ZERO(&rfds);
-	    
+
             // 把标准输入(stdin)句柄0加入到集合中
             FD_SET(0, &rfds);
-    
-            // 把当前连接(socket)句柄new_fd加入到集合中 
+
+            // 把当前连接(socket)句柄new_fd加入到集合中
             FD_SET(new_fd, &rfds);
-	    
-	    maxfd = 0;
+
+	        maxfd = 0;
             if (new_fd > maxfd)
-	    {
                 maxfd = new_fd;
-	    }
-	    
-            // 设置最大等待时间 
+
+            // 设置最大等待时间
             tv.tv_sec = 5;
             tv.tv_usec = 0;
-	    
-            // 开始等待 
+
+            // 开始等待
             retval = select(maxfd + 1, &rfds, NULL, NULL, &tv);
-	    
-            if (retval == -1) 
-	    {
+
+            if (retval == -1) {
                 printf("将退出，select出错！ %s", strerror(errno));
                 break;
-            } 
-            else if (retval == 0) 
-	    {
+            } else if (retval == 0) {
                 printf("没有任何消息到来，用户也没有按键，继续等待……\n");
                 continue;
-            } 
-            else 
-	    {
-		 // 判断当前IO是否是stdin
-                if (FD_ISSET(0, &rfds))  // 用户按键了，则读取用户输入的内容发送出去
-		{                  
+            } else {
+		        // 判断当前IO是否是stdin
+                if (FD_ISSET(0, &rfds)) {    // 用户按键了，则读取用户输入的内容发送出去
                     bzero(buf, MAXBUF + 1);
                     fgets(buf, MAXBUF, stdin);
-                    if (!strncasecmp(buf, "quit", 4)) 
-		    {
+                    if (!strncasecmp(buf, "quit", 4)) {
                         printf("自己请求终止聊天！\n");
                         break;
                     }
+
                     len = send(new_fd, buf, strlen(buf) - 1, 0);
+
                     if (len > 0)
                         printf("消息:%s\t发送成功，共发送了%d个字节！\n", buf, len);
-                    else 
-		    {
+                    else {
                         printf("消息'%s'发送失败！错误代码是%d，错误信息是'%s'\n", buf, errno, strerror(errno));
                         break;
                     }
                 }
-                
-		 // 判断当前IO是否是来自socket
-                if (FD_ISSET(new_fd, &rfds)) // 当前连接的socket上有消息到来则接收对方发过来的消息并显示 
-		{                 
+
+		        // 判断当前IO是否是来自socket
+                if (FD_ISSET(new_fd, &rfds)) {  // 当前连接的socket上有消息到来则接收对方发过来的消息并显示
+
                     bzero(buf, MAXBUF + 1);
-                    // 接收客户端的消息 
+                    // 接收客户端的消息
                     len = recv(new_fd, buf, MAXBUF, 0);
                     if (len > 0)
-		    {
                         printf("接收消息成功:'%s'，共%d个字节的数据\n", buf, len);
-		    }
-                    else 
-		    {
-                        if (len < 0)
+                    else {
+                        if ((ssize_t)len < 0)
                             printf("消息接收失败！错误代码是%d，错误信息是'%s'\n", errno, strerror(errno));
                         else
                             printf("对方退出了，聊天终止\n");
@@ -167,17 +144,16 @@ int main(int argc, char **argv)
                 }
             }
         }
-        
+
         close(new_fd);
         // 处理每个新连接上的数据收发结束
-	
+
         printf("还要和其它连接聊天吗？(no->退出)");
         fflush(stdout);
-	
+
         bzero(buf, MAXBUF + 1);
         fgets(buf, MAXBUF, stdin);
-        if (!strncasecmp(buf, "no", 2))
-	{
+        if (!strncasecmp(buf, "no", 2)) {
             printf("终止聊天！\n");
             break;
         }
